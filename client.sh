0895f22ec2fd4d463a987d7fef03060889477a1c
Wenting Tan <wentingt@umich.edu>
2017-09-04 15:17:17 -0400

    a

---
+++ client.sh
@@ -0,0 +1,1 @@
+ mvn exec:java -Dexec.mainClass="de.uulm.vs.client.DiscardClient" -Dexec.args="localhost 8080 1000"
---
+++ server.sh
@@ -0,0 +1,2 @@
+#mvn exec:java -Dexec.mainClass="de.uulm.vs.server.DiscardServer"
+mvn exec:java -Dexec.mainClass="de.uulm.vs.server.HttpStaticFileServer"
